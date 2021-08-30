21ee55bdfd67d91e738a414a0ae3ad7e037ef41e
Neucar Agrivane <nagrivan@oa-h1.msk.21-school.ru>
2021-08-31 00:45:33 +0300

    segfault

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: nagrivan <nagrivan@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 20:11:55 by nagrivan          #+#    #+#             */
-/*   Updated: 2021/08/27 19:36:01 by nagrivan         ###   ########.fr       */
+/*   Updated: 2021/08/31 00:32:53 by nagrivan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,18 @@
 # include <time.h>
 # include <stdlib.h>
 
+typedef struct s_philo
+{
+	int num_phil; //номер философа
+	int left_fork; // левая вилка
+	int right_fork; // правая вилка
+	int num_eat; // сколько философ пожрал
+	time_t start_eat; // время начала жратвы
+	time_t start_sleep; // время начала сна
+	pthread_t tread;
+	struct s_info *data;
+} t_philo;
+
 typedef struct s_info
 {
 	int number_philo; // количество философов
@@ -30,21 +42,9 @@ typedef struct s_info
 	time_t time_to_start; // время начала
 	time_t current_time; // текущее время
 	t_philo *philo;
-	pthread_mutex_t *forks;
+	pthread_mutex_t *forks; //массив вилок
 	pthread_mutex_t print_mess; // бронь на вывод сообщения
 } t_info;
-
-typedef struct s_philo
-{
-	int num_phil; //номер философа
-	int left_fork; // левая вилка
-	int right_fork; // правая вилка
-	int num_eat; // сколько философ пожрал
-	time_t start_eat; // время начала жратвы
-	time_t start_sleep; // время начала сна
-	pthread_t tread;
-	t_info data;
-} t_philo;
 
 int main(int argc, char **argv);
 int	ft_atoi(const char *str);
--- philo/philo_start.c
+++ philo/philo_start.c
@@ -6,7 +6,7 @@
 /*   By: nagrivan <nagrivan@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 20:05:45 by nagrivan          #+#    #+#             */
-/*   Updated: 2021/08/27 19:39:52 by nagrivan         ###   ########.fr       */
+/*   Updated: 2021/08/31 00:40:06 by nagrivan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,67 +18,97 @@ void *round_life(void *philo)
 	
 	copy_ph = (t_philo *)philo;
 	if (copy_ph->num_phil % 2 == 0)
-		usleep(50);
+		usleep(1000);
 	while (1)
 	{
-		pthread_mutex_lock(&copy_ph->data.forks[copy_ph->left_fork]);
-		copy_ph->data.current_time = get_time();
-		print_messange(copy_ph->data.current_time, copy_ph->num_phil, "take a left fork.");
-		pthread_mutex_lock(&copy_ph->data.forks[copy_ph->right_fork]);
-		copy_ph->data.current_time = get_time();
-		print_messange(copy_ph->data.current_time, copy_ph->num_phil, "take a right fork.");
+		// write(1, "start\n", 7);
+		pthread_mutex_lock(&copy_ph->data->forks[copy_ph->left_fork]);
+		copy_ph->data->current_time = get_time();
+		pthread_mutex_lock(&copy_ph->data->print_mess);
+		print_messange(copy_ph->data->current_time, copy_ph->num_phil, "take a left fork.");
+		pthread_mutex_unlock(&copy_ph->data->print_mess);
+		pthread_mutex_lock(&copy_ph->data->forks[copy_ph->right_fork]);
+		copy_ph->data->current_time = get_time();
+		pthread_mutex_lock(&copy_ph->data->print_mess);
+		print_messange(copy_ph->data->current_time, copy_ph->num_phil, "take a right fork.");
+		pthread_mutex_unlock(&copy_ph->data->print_mess);
 		copy_ph->num_eat++;
 		copy_ph->start_eat = get_time();
+		pthread_mutex_lock(&copy_ph->data->print_mess);
 		print_messange(copy_ph->start_eat, copy_ph->num_phil, "is eating.");
-		ft_usleep(copy_ph->data.time_to_eat);
-		pthread_mutex_unlock(&copy_ph->data.forks[copy_ph->left_fork]);
-		pthread_mutex_unlock(&copy_ph->data.forks[copy_ph->right_fork]);
+		pthread_mutex_unlock(&copy_ph->data->print_mess);
+		ft_usleep(copy_ph->data->time_to_eat);
+		pthread_mutex_unlock(&copy_ph->data->forks[copy_ph->left_fork]);
+		pthread_mutex_unlock(&copy_ph->data->forks[copy_ph->right_fork]);
 		copy_ph->start_sleep = get_time();
+		pthread_mutex_lock(&copy_ph->data->print_mess);
 		print_messange(copy_ph->start_sleep, copy_ph->num_phil, "is sleeping.");
-		ft_usleep(copy_ph->data.time_to_sleep);
-		print_messange(copy_ph->data.current_time, copy_ph->num_phil, "if thinking.");
+		pthread_mutex_unlock(&copy_ph->data->print_mess);
+		ft_usleep(copy_ph->data->time_to_sleep);
+		pthread_mutex_lock(&copy_ph->data->print_mess);
+		print_messange(copy_ph->data->current_time, copy_ph->num_phil, "if thinking.");
+		pthread_mutex_unlock(&copy_ph->data->print_mess);
+		// write(1, "end\n", 5);
 	}
 }
 
-void start_play(t_info *info, t_philo *philo)
+void start_play(t_info *info)
 {
 	int i;
 
 	i = 1;
 	while (i <= info->number_philo)
 	{
-		pthread_create(&philo[i].tread, NULL, &round_life, (void *)(&philo[i]));
-		pthread_detach(philo[i].tread);
-	}
-}
-
-void init_phill(t_info *info, t_philo *philo) // инициализируем структуру каждого философа
-{
-	int i;
-
-	i = 1;
-	while (i <= info->number_philo)
-	{
-		philo[i].num_phil = i;
-		philo->left_fork = philo->num_phil - 1;
-		if (philo->left_fork < 1)
-			philo->left_fork = info->number_philo;
-		philo->right_fork = philo->num_phil;
-		pthread_mutex_init(&info->forks[philo->left_fork], NULL);
-		pthread_mutex_init(&info->forks[philo->right_fork], NULL);
-		philo[i].num_eat = 0;
+		// write(1, "error4.1\n", 10);
+		pthread_create(&info->philo[i].tread, NULL, &round_life, (void *)(&info->philo[i]));
+		// write(1, "error4.2\n", 10);
+		pthread_detach(info->philo[i].tread);
+		// write(1, "error4.3\n", 10);
 		i++;
 	}
 }
 
-int before_a_game(t_info *info, t_philo *philo) // подготовка к началу "голодных игр" плодим необходимое количество структур под каждого философа и узнаем время начала.
+void init_phill(t_info *info, int i) // инициализируем структуру каждого философа
 {
-	philo = (t_philo *)ft_calloc(sizeof(t_philo), info->number_philo);
-	if (!philo)
+	// while (i <= info->number_philo)
+	// {
+		// printf("number_philo = %d num_phil = %d\n", info->number_philo, info->philo[i].num_phil);
+		info->philo[i].num_phil = i + 1;
+		// printf("number_philo = %d num_phil = %d\n", info->number_philo, philo[i]->num_eat);
+		info->philo[i].left_fork = info->philo[i].num_phil - 1;
+		if (info->philo[i].left_fork < 1)
+			info->philo[i].left_fork = info->number_philo;
+		info->philo[i].right_fork = info->philo[i].num_phil;
+		// pthread_mutex_init(&info->forks[info->philo[i].left_fork], NULL);
+		// pthread_mutex_init(&info->forks[info->philo[i].right_fork], NULL);
+		info->philo[i].num_eat = 0;
+	// 	i++;
+	// }
+}
+
+int before_a_game(t_info *info) // подготовка к началу "голодных игр" плодим необходимое количество структур под каждого философа и узнаем время начала.
+{
+	int i;
+	
+	i = 0;
+	info->philo = (t_philo *)ft_calloc(sizeof(t_philo), info->number_philo);
+	if (!info->philo)
 		return (1);
 	info->forks = (pthread_mutex_t *)ft_calloc(sizeof(pthread_mutex_t), info->number_philo);
 	if (!info->forks)
 		return (1);
+	while (i < info->number_philo)
+	{
+		pthread_mutex_init(&info->forks[i], NULL);
+		i++;
+	}
+	i = 0;
+	while (i < info->number_philo)
+	{
+		init_phill(info, i);
+		i++;
+	}
+	pthread_mutex_init(&info->print_mess, NULL);
 	info->time_to_start = get_time();
 	return (0);
 }
@@ -103,14 +133,16 @@ int init_struct(t_info *info, int argc, char **argv) // инициализаци
 int main(int argc, char **argv)
 {
 	t_info info;
-	t_philo *philo;
 	
 	if (argc != 5 && argc != 6)
 		return (1);
 	if ((init_struct(&info, argc, argv)) != 0)
 		return (1);
-	before_a_game(&info, &philo);
-	init_phill(&info, &philo);
-	start_play(&info, &philo);
+	// write(1, "error1\n", 8);
+	before_a_game(&info);
+	// write(1, "error2\n", 8);
+	// write(1, "error3\n", 8);
+	start_play(&info);
+	// write(1, "error4\n", 8);
 	return (0);
 }
